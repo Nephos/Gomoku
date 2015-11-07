889e8367d102d940168370084f350fdb6ccb16a8
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-11-07 21:36:39 +0100

    add documentation for map

--- Server/app/models/map.rb
+++ Server/app/models/map.rb
@@ -7,6 +7,7 @@ class Map
     @data = Array.new(19){Array.new(19) {nil}}
   end
 
+  # we can use map like an array
   def method_missing m, *a
     @data.send(m, *a)
   end
@@ -15,12 +16,16 @@ class Map
     @data
   end
 
+  # T is the list of the direction, an array of [y, x]
   T = [
     [-1, -1], [-1, 0], [-1, 1],
     [0, -1], [0, 1],
     [1, -1], [1, 0], [1, 1],
   ]
+
   public
+  # try to take every lines around (y, x)
+  # use the directions T
   def take_around! y, x, color
     T.each do |tuple|
       y2, x2 = y + tuple[0], x + tuple[1]
@@ -32,11 +37,15 @@ class Map
       end
     end
   end
+
+  # change elements "2" to "color"
   def took! color
     @data.map!{|line| line.map{|e| e == 2 ? color : e}}
   end
 
   private
+  # try to take the line with the direction "tuple"
+  # from (y, x)
   def take_direction! tuple, y, x, color
     #puts "Check at #{y}:#{x}: #{@data[y][x].class}"
     case @data[y][x]
@@ -54,6 +63,9 @@ class Map
   end
 
   public
+  # for every "color" or "2"
+  # test with every lines, based on T and (y, x)
+  # if there is a 5 aligned "color" or "2"
   def win? color
     @data.each_with_index do |line, y|
       line.each_with_index do |e, x|
@@ -69,6 +81,7 @@ class Map
   end
 
   private
+  # test a line
   def win_direction? tuple, y, x, color, distance=0
     return true if distance == 4
     case @data[y][x]
