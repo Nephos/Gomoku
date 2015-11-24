12c826e361e76a078091ffff8db7fa0c48a01eef
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-11-24 17:01:39 +0100

    update tests

--- Server/test/game_real.rb
+++ Server/test/game_real.rb
@@ -33,14 +33,14 @@ class TestGameReal < Test::Unit::TestCase
   def test_game_simple
     new_game
     play(1, 0, 0, 200, 200)
-    play(2, 1, 0, 200, 200)
-    play(1, 0, 1, 200, 200)
-    play(2, 2, 0, 200, 200)
-    play(1, 0, 2, 200, 200)
-    play(2, 3, 0, 200, 200)
-    play(1, 0, 3, 200, 200)
-    play(2, 4, 0, 200, 200)
-    play(1, 0, 4, 200, 200, {end_msg: "You win."})
+    play(2, 0, 1, 200, 200)
+    play(1, 1, 0, 200, 200)
+    play(2, 0, 2, 200, 200)
+    play(1, 2, 0, 200, 200)
+    play(2, 0, 3, 200, 200)
+    play(1, 3, 0, 200, 200)
+    play(2, 0, 4, 200, 200)
+    play(1, 4, 0, 200, 200, {end_msg: "You win."})
   end
 
   def test_game_simple_map
@@ -51,35 +51,35 @@ class TestGameReal < Test::Unit::TestCase
     map[0][0] = 0
     assert_equal map.to_a, get_map
 
-    play(2, 1, 0, 200, 200)
+    play(2, 0, 1, 200, 200)
     map[0][1] = 1
     assert_equal map.to_a, get_map
 
-    play(1, 0, 1, 200, 200)
+    play(1, 1, 0, 200, 200)
     map[1][0] = 0
     assert_equal map.to_a, get_map
 
-    play(2, 2, 0, 200, 200)
+    play(2, 0, 2, 200, 200)
     map[0][2] = 1
     assert_equal map.to_a, get_map
 
-    play(1, 0, 2, 200, 200)
+    play(1, 2, 0, 200, 200)
     map[2][0] = 0
     assert_equal map.to_a, get_map
 
-    play(2, 3, 0, 200, 200)
+    play(2, 0, 3, 200, 200)
     map[0][3] = 1
     assert_equal map.to_a, get_map
 
-    play(1, 0, 3, 200, 200)
+    play(1, 3, 0, 200, 200)
     map[3][0] = 0
     assert_equal map.to_a, get_map
 
-    play(2, 4, 0, 200, 200)
+    play(2, 0, 4, 200, 200)
     map[0][4] = 1
     assert_equal map.to_a, get_map
 
-    body = play(1, 0, 4, 200, 200).body.to_s
+    body = play(1, 4, 0, 200, 200).body.to_s
     map[4][0] = 0
     assert_equal map.to_a, get_map
     assert_equal "You win.", JSON.parse(body)["message"]
@@ -89,10 +89,10 @@ class TestGameReal < Test::Unit::TestCase
     new_game
     map = Map.new
     play(1, 0, 0, 200, 200); map[0][0] = 0
-    play(2, 0, 1, 200, 200); map[1][0] = 1
-    play(1, 0, 9, 200, 200); map[9][0] = 0
-    play(2, 0, 2, 200, 200); map[2][0] = 1
-    play(1, 0, 3, 200, 200); map[3][0] = 0
+    play(2, 1, 0, 200, 200); map[1][0] = 1
+    play(1, 9, 0, 200, 200); map[9][0] = 0
+    play(2, 2, 0, 200, 200); map[2][0] = 1
+    play(1, 3, 0, 200, 200); map[3][0] = 0
     map[1][0] = nil
     map[2][0] = nil
     assert_equal map.to_a, get_map
@@ -101,26 +101,64 @@ class TestGameReal < Test::Unit::TestCase
   def test_double_take
     new_game
     map = Map.new
-    play_map(map, 1, 0, 1) # 1
+    play_map(map, 1, 1, 0) # 1
     play_map(map, 2, 1, 1)
-    play_map(map, 1, 1, 0) # 2
-    play_map(map, 2, 2, 1)
-    play_map(map, 1, 0, 0) # 3
-    play_map(map, 2, 3, 1)
-    play_map(map, 1, 4, 1) # 4
+    play_map(map, 1, 0, 1) # 2
     play_map(map, 2, 1, 2)
-    _ = play_map(map, 1, 1, 3).body.to_s # 5
+    play_map(map, 1, 0, 0) # 3
+    play_map(map, 2, 1, 3)
+    play_map(map, 1, 1, 4) # 4
+    play_map(map, 2, 2, 1)
+    _ = play_map(map, 1, 3, 2).body.to_s # 5
     assert_equal 0, get_map.count(1)
     # continue
   end
 
-  def play(color, x, y, status_wait=nil, status_end=nil, opt={})
+  def test_5_breakable
+    new_game
+    map = Map.new
+    assert_equal map.to_a, get_map
+    play_map(map, :white, 1, 0, 200, 200)
+    play_map(map, :black, 9, 0, 200, 200)
+
+    play_map(map, :white, 1, 1, 200, 200)
+    play_map(map, :black, 9, 1, 200, 200)
+
+    play_map(map, :white, 1, 2, 200, 200)
+    play_map(map, :black, 9, 2, 200, 200)
+
+    play_map(map, :white, 2, 2, 200, 200)
+    play_map(map, :black, 0, 2, 200, 200)
+
+    play_map(map, :white, 1, 3, 200, 200)
+    play_map(map, :black, 9, 3, 200, 200)
+
+    # not win
+    play_map(map, :white, 1, 4, 200, 200)
+
+    # forbidden -> 5 breakable
+    require 'pry'; binding.pry
+    play(:black, 4, 0, 200, 401)#; map[5][0] = 1
+    # mandatory -> 5 breakable
+    require 'pry'; binding.pry
+    play(:black, 3, 2, 200, 200); map[3][2] = 1
+
+    assert_equal map.to_a, get_map
+    assert_equal "You win.", JSON.parse(body)["message"]
+  end
+
+
+
+  private
+  def play(color, y, x, status_wait=nil, status_end=nil, opt={})
+    color = 1 if color == :white
+    color = 2 if color == :black
     c = (color == 1 ? @c1 : @c2)
     co = (color == 1 ? @co1 : @co2)
     r = c.get(WAIT, {:headers => co})
     assert_equal status_wait, r.status if status_wait
     r = c.post(PLAY.gsub('X', x.to_s).gsub('Y', y.to_s), {headers: co})
-    puts r.body.to_s
+    puts r.body.to_s if not r.status == 200
     assert_equal status_end, r.status if status_end
     if opt[:end_msg]
       assert_equal JSON.parse(r.body.to_s)["message"], opt[:end_msg]
@@ -128,10 +166,15 @@ class TestGameReal < Test::Unit::TestCase
     r
   end
 
-  def play_map(map, color, x, y, status_wait=nil, status_end=nil, opt={})
-    r = play(color, x, y, status_wait, status_end, opt)
+  def play_map(map, color, y, x, status_wait=nil, status_end=nil, opt={})
+    color = 1 if color == :white
+    color = 2 if color == :black
+    r = play(color, y, x, status_wait, status_end, opt)
     if r.status == 200
       map[y][x] = color - 1
+      map.update!(y, x)
+      map.take_around!(y, x, color - 1)
+      map.moves = []
     end
     r
   end
