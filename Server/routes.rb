e04bc1b1e8981e527c32faa1567156f3c5ad285c
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-01-24 15:14:09 +0100

    add wait route

--- Server/routes.rb
+++ Server/routes.rb
@@ -4,6 +4,7 @@ end
 
 resource "/game" do
   get "/", to: "GameController#request_round"
+  get "/wait", to: "GameController#request_round"
   get "/map", to: "GameController#request_map"
   post "/play/:x/:y", to: "GameController#play_round"
   get "/play/:x/:y", to: "GameController#play_round" # DEBUG
